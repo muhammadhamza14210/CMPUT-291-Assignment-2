8f3c5a4cef7eb196a1e497448eab46e2360fec26
Muhammad Hamza <63776709+muhammadhamza14210@users.noreply.github.com>
2023-02-16 12:51:01 -0700

    Update 7.sql

--- 7.sql
+++ 7.sql
@@ -1,4 +1,6 @@
 --Question 7 mhamza2
-SELECT DISTINCT(I.Title)
-FROM IMDBMovie I, NPHDMovies N
-WHERE I.Title != N.Title AND instr(I.Actors,I.Director)
+SELECT Title
+FROM IMDBMovie I
+WHERE I.Title NOT IN (
+						SELECT Title from NPHDMovies N) 
+						AND INSTR(I.Actors, I.Director);
