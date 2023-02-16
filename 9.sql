76bdfc72198589c9052a00601cbf0b95aec802fe
Muhammad Hamza <63776709+muhammadhamza14210@users.noreply.github.com>
2023-02-16 12:51:59 -0700

    Update 9.sql

--- 9.sql
+++ 9.sql
@@ -1,28 +1,20 @@
 --Question 9 mhamza2
 SELECT *
-FROM ( SELECT 'Netflix' as Platform, N.Title, I.Revenue
+FROM ( SELECT 'Netflix' as Platform, N.Title,MAX(I.Revenue)
 				 FROM IMDBMovie I, NPHDMovies N
-				 WHERE I.Title = N.Title AND N.Netflix = 1 AND N.Disney = 0 AND N.PrimeVideo = 0 AND N.Hulu = 0
-				 ORDER BY I.Revenue DESC
-				 LIMIT 1)
+				 WHERE I.Title = N.Title AND N.Netflix = 1)
 UNION
 SELECT *
-FROM ( SELECT 'Hulu' as Platform, N.Title, I.Revenue
+FROM ( SELECT 'Hulu' as Platform, N.Title,MAX(I.Revenue)
 				 FROM IMDBMovie I, NPHDMovies N
-				 WHERE I.Title = N.Title AND N.Netflix = 0 AND N.Disney = 0 AND N.PrimeVideo = 0 AND N.Hulu = 1
-				 ORDER BY I.Revenue DESC
-				 LIMIT 1)
+				 WHERE I.Title = N.Title AND  N.Hulu = 1)
 UNION
 SELECT *
-FROM ( SELECT 'Disney' as Platform, N.Title, I.Revenue
+FROM ( SELECT 'Disney' as Platform, N.Title,MAX(I.Revenue)
 				 FROM IMDBMovie I, NPHDMovies N
-				 WHERE I.Title = N.Title AND N.Netflix = 0 AND N.Disney = 1 AND N.PrimeVideo = 0 AND N.Hulu = 0
-				 ORDER BY I.Revenue DESC
-				 LIMIT 1)
+				 WHERE I.Title = N.Title AND N.Disney = 1)
 UNION
 SELECT *
-FROM ( SELECT 'PrimeVideo' as Platform, N.Title, I.Revenue
+FROM ( SELECT 'PrimeVideo' as Platform, N.Title, MAX(I.Revenue)
 				 FROM IMDBMovie I, NPHDMovies N
-				 WHERE I.Title = N.Title AND N.Netflix = 0 AND N.Disney = 0 AND N.PrimeVideo = 1 AND N.Hulu = 0
-				 ORDER BY I.Revenue DESC
-				 LIMIT 1)
+				 WHERE I.Title = N.Title AND  N.PrimeVideo = 1 )
